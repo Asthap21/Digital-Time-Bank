d5785bdddcd508d4b32634a27bf3eebcc78e3135
Asthap21 <asthapatwa3@gmail.com>
2025-05-04 10:14:34 +0530

    Add mobile column to user model

---
+++ migrations/versions/0f3ab2ab2704_added_mobile_column_to_user_model.py
@@ -0,0 +1,24 @@
+"""Added mobile column to User model
+
+Revision ID: 0f3ab2ab2704
+Revises: 0372ee5d221e
+Create Date: 2025-05-03 16:44:29.399957
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0f3ab2ab2704'
+down_revision = '0372ee5d221e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
